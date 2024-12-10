c1e73572a08c273432cd6bd5aca11eccb3b4dc5b
alexNazarenkoDev <alex.nazarenko.dev@gmail.com>
2024-12-10 05:31:15 +0200

    add post page

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -6,6 +6,17 @@ $('.header-btn').on('click', function (e) {
     $('.header').toggleClass('is-open');
     $('body').toggleClass('is-scroll');
 });
+$('.tab-nav a').on('click', function (e) {
+    e.preventDefault();
+    const $ths = $(this);
+    const $thsHref = $ths.attr('data-tab-href');
+
+    $ths.closest('.tabs').find('.tab-nav a').removeClass('active');
+    $ths.closest('.tabs').find('.tab-inner div[data-tab]').removeClass('active');
+
+    $ths.addClass('active');
+    $ths.closest('.tabs').find('div[data-tab="' + $thsHref + '"]').addClass('active');
+});
 $('.software-slider').slick({
     dots: true,
     infinite: true,
